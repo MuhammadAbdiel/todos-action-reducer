236d513e00615a77db0c532e6bd2579d571b154e
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-12-18 14:13:50 +0700

    create action and reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+function createStore() {
+  /**
+   * Store memiliki 4 hal
+   * 1. State (done)
+   * 2. Mendapatkan state (done)
+   * 3. Men-subscribe perubahan state (done)
+   * 4. Memperbarui state
+   */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  //  the store
+  return {
+    getState,
+    subscribe,
+  };
 }
 
-export default App;
+function addTodoActionCreator({ id, text }) {
+  return {
+    type: "ADD_TODO",
+    payload: {
+      id,
+      text,
+      complete: false,
+    },
+  };
+}
+
+function todosReducer(todos = [], action) {
+  // First way
+  // switch (action.type) {
+  //   case "ADD_TODO":
+  //     return todos.concat(action.payload);
+  //   default:
+  //     return todos;
+  // }
+
+  // Second way
+  // switch (action.type) {
+  //   case "ADD_TODO":
+  //     return [...todos, action.payload];
+  //   default:
+  //     return todos;
+  // }
+
+  // Third way
+  if (action.type === "ADD_TODO") {
+    return [...todos, action.payload];
+  }
+
+  return todos;
+}
+
+// consume
+const store = createStore();
+
+// subscribe state changed
+const unsubscribe = store.subscribe(() => {
+  console.log("State changed");
+});
+
+// unsubscribe
+unsubscribe();
+
+// getting the state
+store.getState();
